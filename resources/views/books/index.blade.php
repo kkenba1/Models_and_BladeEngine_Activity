3881528546172af999a7a0da64c7042b3ecf0a34
unknown <kar2730@students.uc-bcf.edu.ph>
2025-04-01 22:19:34 +0800

    added id column to book list output

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -5,6 +5,7 @@
         <table class="min-w-full border border-gray-300">
             <thead>
                 <tr class="bg-gray-200">
+                    <th class="border px-4 py-2">ID</th>
                     <th class="border px-4 py-2">ISBN</th>
                     <th class="border px-4 py-2">Title</th>
                     <th class="border px-4 py-2">Author</th>
@@ -15,6 +16,7 @@
             <tbody>
                 @foreach($books as $book)
                     <tr>
+                        <td class="border px-4 py-2">{{ $book->id }}</td>
                         <td class="border px-4 py-2">{{ $book->isbn }}</td>
                         <td class="border px-4 py-2">{{ $book->title }}</td>
                         <td class="border px-4 py-2">{{ $book->author }}</td>
